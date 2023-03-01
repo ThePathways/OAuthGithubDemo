0dfa5b6bf1c583fad06fc0c8ce889d2ddd71eda1
ThePathways <34189432+ThePathways@users.noreply.github.com>
2023-03-01 14:33:05 +0800

    Initial commit

---
+++ src/main/java/com/thepathways/oauthgithubdemo/OAuthGithubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.thepathways.oauthgithubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthGithubDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthGithubDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thepathways/oauthgithubdemo/OAuthGithubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thepathways.oauthgithubdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthGithubDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
