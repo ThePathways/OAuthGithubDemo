f3839deb4a7022aa6678e9f7bfe07c5fe8e856d7
Piyumi Gunarathna <piyumi.gunarathna@Zuhlke.com>
2023-03-07 10:09:51 +0800

    added public controller to access without authentication

---
+++ src/main/java/com/thepathways/oauthgithubdemo/controller/PublicController.java
@@ -0,0 +1,19 @@
+package com.thepathways.oauthgithubdemo.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/public/")
+public class PublicController {
+
+    @GetMapping("/greeting")
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello you haven't login yet");
+    }
+
+
+}
